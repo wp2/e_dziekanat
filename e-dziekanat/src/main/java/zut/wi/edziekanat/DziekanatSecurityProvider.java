7441dd21204d219d551bdc4707e4920a6dedf83b
Wojciech Pardel <wpardel@gmail.com>
2017-01-06 22:14:20 +0100

    Update 0.5
    + Dodanie szablonu StudentLayout na którym będą się opierać inne widoki
    w warstwie Studenta
    
    + Pierwsza prowizoryczna konfiguacja Security

--- e-dziekanat/src/main/java/zut/wi/edziekanat/DziekanatSecurityProvider.java
+++ e-dziekanat/src/main/java/zut/wi/edziekanat/DziekanatSecurityProvider.java
@@ -1,21 +1,73 @@
 package zut.wi.edziekanat;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
 
-public class DziekanatSecurityProvider implements AuthenticationProvider {
+import zut.wi.edziekanat.services.StudentService;
+
+@Component
+public class DziekanatSecurityProvider implements AuthenticationProvider 
+{
+	@Autowired
+	HttpServletRequest httpRequest;
+	
+	@Autowired
+	StudentService studentService;
 
 	@Override
-	public Authentication authenticate(Authentication arg0) throws AuthenticationException {
-		// TODO Auto-generated method stub
-		return null;
+	public Authentication authenticate(Authentication auth) throws AuthenticationException 
+	{
+		
+		String [] loginType = httpRequest.getParameterValues("LoginType"); // Typ Logowania
+		List<GrantedAuthority> grantedAuthority = new ArrayList<GrantedAuthority>();
+		grantedAuthority.add(new SimpleGrantedAuthority("ROLE_STUDENT")); 
+		String Login = auth.getName();
+		String Passwd = auth.getCredentials().toString();
+		return new UsernamePasswordAuthenticationToken("Hello","Passwd",grantedAuthority);	
+		/*if( loginType[0] != null && loginType[0].equals("Student") )
+		{
+			String Login = auth.getName();
+			String Passwd = auth.getCredentials().toString();
+			if(studentService.getUserNamePassword(Login).equals(Passwd))
+			{
+				grantedAuthority.add(new SimpleGrantedAuthority("ROLE_STUDENT")); // Uprawnienia Studenta
+				// TO DO Hook up service DB Pass AUTH 
+				return new UsernamePasswordAuthenticationToken(Login,Passwd,grantedAuthority);		
+			}
+			else
+			{
+				return null;
+			}
+						
+		}
+		else if(loginType[0].equals("Dydaktyk"))
+		{
+			return new UsernamePasswordAuthenticationToken("C", "d",grantedAuthority);
+		}
+		else
+		{
+			return null;
+		}*/
+		
+		
 	}
 
 	@Override
-	public boolean supports(Class<?> arg0) {
+	public boolean supports(Class<?> auth) 
+	{
 		// TODO Auto-generated method stub
-		return false;
+		return auth.equals(UsernamePasswordAuthenticationToken.class);
 	}
 
 }
--- e-dziekanat/src/main/java/zut/wi/edziekanat/dao/StudentDao.java
+++ e-dziekanat/src/main/java/zut/wi/edziekanat/dao/StudentDao.java
@@ -3,6 +3,11 @@ package zut.wi.edziekanat.dao;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class StudentDao {
+public class StudentDao 
+{
+	public String getUserNamePassword(String Username)
+	{
+		return "Passwd";
+	}
 
 }
